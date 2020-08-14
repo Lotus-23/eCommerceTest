e540fba33ce44e009f471700bcacdc04e53909de
Javier Rozas <lotus23ideas@gmail.com>
2020-08-14 15:12:45 -0400

    Pagina detalle producto + minor fixes + wired

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ function menuToggle(){
         MenuItems.style.maxHeight = "0px"
     }
 }
+
+// Selector imagenes pagina de productos
+
+const ProductImg = document.getElementById("product-img");
+const SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function() {
+    ProductImg.src = SmallImg[0].src;
+}
+SmallImg[1].onclick = function() {
+    ProductImg.src = SmallImg[1].src;
+}
+SmallImg[2].onclick = function() {
+    ProductImg.src = SmallImg[2].src;
+}
+SmallImg[3].onclick = function() {
+    ProductImg.src = SmallImg[3].src;
+}
+
